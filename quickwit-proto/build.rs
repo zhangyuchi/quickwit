7031bb87edffb91105c19e58ed2150a89d7316f1
Paul Masurel <paul@quickwit.io>
2022-03-09 11:55:41 +0900

    Protobuf templ generator

--- quickwit-proto/build.rs
+++ quickwit-proto/build.rs
@@ -20,6 +20,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=proto/cluster.proto");
     println!("cargo:rerun-if-changed=proto/search_api.proto");
+    println!("cargo:rerun-if-changed=SERVICE.rs.tmpl");
 
     let mut prost_config = prost_build::Config::default();
     // prost_config.type_attribute("LeafSearchResponse", "#[derive(Default)]");
@@ -36,5 +37,71 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             &["./proto/cluster.proto", "./proto/search_api.proto"],
             &["./proto"],
         )?;
+
+    prost_build::Config::default()
+        .service_generator(Box::new(QuickwitServiceGenerator))
+        .out_dir("service/")
+        .compile_protos( &[
+                "./proto/cluster.proto",
+                "./proto/search_api.proto",
+        ],
+        &["./proto"])?;
     Ok(())
+}
+
+use serde::Serialize;
+/// A service descriptor.
+#[derive(Serialize)]
+struct Service {
+    /// The service name in Rust style.
+    pub service_name: String,
+    /// The service methods.
+    pub methods_streaming: Vec<Method>,
+    pub methods_non_streaming: Vec<Method>,
+}
+
+#[derive(Serialize)]
+struct Method {
+    /// The name of the method in Rust style.
+    pub name: String,
+    pub input_type: String,
+    pub output_type: String,
+    // pub client_streaming: bool,
+    // pub server_streaming: bool,
+}
+
+impl From<prost_build::Method> for Method {
+    fn from(method: prost_build::Method) -> Self {
+        Method {
+            name: method.name,
+            input_type: method.input_type,
+            output_type: method.output_type,
+            // server_streaming: method.server_streaming
+        }
+    }
+}
+
+impl From<prost_build::Service> for Service {
+    fn from(service: prost_build::Service) -> Self {
+        let (streaming_methods, non_streaming_methods): (Vec<_>, Vec<_>) = service.methods
+            .into_iter()
+            .partition(|method| method.server_streaming);
+        Service {
+            service_name: service.name,
+            methods_streaming: streaming_methods.into_iter().map(Method::from).collect(),
+            methods_non_streaming: non_streaming_methods.into_iter().map(Method::from).collect(),
+        }
+    }
+}
+
+const SERVICE_TEMPLATE: &str = include_str!("SERVICE.rs.tmpl");
+
+struct QuickwitServiceGenerator;
+
+impl prost_build::ServiceGenerator for QuickwitServiceGenerator {
+    fn generate(&mut self, service: prost_build::Service, buf: &mut String) {
+        let service = Service::from(service);
+        let tmpl = mustache::compile_str(SERVICE_TEMPLATE).unwrap();
+        buf.push_str(&tmpl.render_to_string(&service).unwrap());
+    }
 }
